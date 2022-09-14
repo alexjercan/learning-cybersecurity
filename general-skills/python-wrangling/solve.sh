cb7d42c6855c4c9a0ac8954b6af3efca57e158ef
Alexandru Jercan <jercan_alex27@yahoo.com>
2022-09-14 21:29:49 +0300

    fix

--- general-skills/python-wrangling/solve.sh
+++ general-skills/python-wrangling/solve.sh
@@ -4,9 +4,9 @@ script_url=https://mercury.picoctf.net/static/1b247b1631eb377d9392bfa4871b2eb1/e
 password_url=https://mercury.picoctf.net/static/1b247b1631eb377d9392bfa4871b2eb1/pw.txt
 flag_url=https://mercury.picoctf.net/static/1b247b1631eb377d9392bfa4871b2eb1/flag.txt.en
 
-wget ${script_url} -O ende.py
-wget ${password_url} -O pw.txt
-wget ${flag_url} -O flag.txt.en
+wget ${script_url} -O ende.py -q
+wget ${password_url} -O pw.txt -q
+wget ${flag_url} -O flag.txt.en -q
 
 cat pw.txt | python ende.py -d flag.txt.en
 
