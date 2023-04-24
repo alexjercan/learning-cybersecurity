43d9b1be3c5158feb10d84c73ba3cc203f8b098e
Alex Jercan <jercan_alex27@yahoo.com>
2023-04-24 20:37:36 +0300

    feat: added tmux sessionizer and category

--- pico-ctf/mk-pico-readme.sh
+++ pico-ctf/mk-pico-readme.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
-if [ "$#" -ne 1 ]; then
-    echo "Usage $0 \"<name>\""
+if [ "$#" -ne 2 ]; then
+    echo "Usage $0 \"<category>\" \"<name>\""
     exit 1
 fi
 
-name=$1
-dirname=$(echo -n $name | tr -c "[:alnum:]" "-" | tr '[:upper:]' '[:lower:]')
+category=$1
+name=$2
+
+prefix=$(echo -n $category | tr -c "[:alnum:]" "-" | tr '[:upper:]' '[:lower:]')
+dirname=$prefix/$(echo -n $name | tr -c "[:alnum:]" "-" | tr '[:upper:]' '[:lower:]')
 
 mkdir -p $dirname
 cd $dirname
 
 echo "# $name\n\n[$name](https://play.picoctf.org/practice/challenge/)\n\n## Description\n\n## Solution\n
 " > README.md
+
+if command -v tmux-sessionizer &> /dev/null
+then
+    tmux-sessionizer $(pwd)
+fi
+
